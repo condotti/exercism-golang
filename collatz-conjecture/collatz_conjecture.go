cf1526266e71fceb1b55fe5e7809bc9414f06dbf
condotti <akondo21@gmail.com>
2019-10-17 11:11:24 +0900

    golint and gofmt applied

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,24 +1,19 @@
 package collatzconjecture
 
-type CollatzError struct {
-	Message string
-}
+import "errors"
 
-func (e *CollatzError) Error() string {
-	return e.Message
-}
-
+// CollatzConjecture returns the number of Collatz steps or an error
 func CollatzConjecture(n int) (int, error) {
 	if n > 0 {
 		steps := 0
 		for ; n != 1; steps++ {
-			if n & 1 == 0 {
+			if n&1 == 0 {
 				n >>= 1
 			} else {
-				n = n << 1 + n + 1
+				n = n<<1 + n + 1
 			}
 		}
 		return steps, nil
 	}
-	return 0, &CollatzError{ "Argument must be positive integer" }
+	return 0, errors.New("Argument must be positive integer")
 }
