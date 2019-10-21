71c9ba748e22b78af6d733beeae5d915f37f1327
condotti <akondo21@gmail.com>
2019-10-21 14:01:04 +0900

    gofmt applied (nucleotide-count)

--- nucleotide-count/nucleotide_count.go
+++ nucleotide-count/nucleotide_count.go
@@ -11,7 +11,7 @@ type DNA string
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram = Histogram{'A': 0, 'C':0, 'G':0, 'T':0 }
+	var h Histogram = Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
 
 	for _, n := range d {
 		_, ok := h[n]
