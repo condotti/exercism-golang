ed5e7c0c398757db7041fd6e5dd40aabe3be56cc
condotti <akondo21@gmail.com>
2019-11-07 16:00:00 +0900

    made faster (reverse-string)

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -4,9 +4,9 @@ package reverse
 // Reverse returns a revresed string.
 func Reverse(a string) string {
 	ar := []rune(a)
-	br := []rune{}
-	for _, c := range ar {
-		br = append([]rune{ c }, br...)
+	br := make([]rune, len(ar))
+	for i := range ar {
+		br[i] = ar[len(ar)-i-1]
 	}
 	return string(br)
 }
