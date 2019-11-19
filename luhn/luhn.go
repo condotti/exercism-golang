a689c02a874ef32b9c0e42e5ecc7e615542a9af0
condotti <akondo21@gmail.com>
2019-11-19 14:12:52 +0900

    mod to prepare submission (luhn)

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,28 +1,29 @@
+// Package luhn implements a solution of the exercise titled `Luhn'.
 package luhn
 
 // Valid determines whether or not NUM is valid per the Luhn formula
 func Valid(num string) bool {
-	digits := []int{}
-	for _, c := range num {
+	doubled := []int{0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
+	rnum := []rune(num)
+	var i, sum int
+	for j := range rnum {
+		c := rnum[len(rnum)-j-1]
 		if c != ' ' {
-			if c >= '0' && c <= '9' {
-				digits = append(digits, int(c-'0'))
+			n := int(c - '0')
+			if 0 <= n && n <= 9 {
+				if i&1 == 0 {
+					sum += n
+				} else {
+					sum += doubled[n]
+				}
+				i++
 			} else {
 				return false
 			}
 		}
 	}
-	if len(digits) <= 1 {
+	if i <= 1 {
 		return false
-	}
-	sum := 0
-	doubled := []int{0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
-	for i := range digits {
-		if i&1 == 0 {
-			sum += digits[len(digits)-i-1]
-		} else {
-			sum += doubled[digits[len(digits)-i-1]]
-		}
 	}
 	return sum%10 == 0
 }
