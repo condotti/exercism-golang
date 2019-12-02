3427fdac8cb824d569391bee31a3e24be4695995
condotti <akondo21@gmail.com>
2019-12-02 13:11:53 +0900

    mod before submit (clock)

--- clock/clock.go
+++ clock/clock.go
@@ -1,30 +1,32 @@
+// Package clock implements a solution of the exercise titled `Clock'.
 package clock
 
 import "fmt"
 
+// Clock represents a clock that handles times without dates.
 type Clock int
 
-// New(H, M) is the constuctor of Clock which represents H:M
+// New is the ctor of Clock.
 func New(h, m int) Clock {
-	m += h*60
-	m %= 60*24
+	m += h * 60
+	m %= 60 * 24
 	if m < 0 {
-		m += 60*24
+		m += 60 * 24
 	}
-	return Clock(m);
+	return Clock(m)
 }
 
-// String() is a stringer for Clock
-func (c Clock)String() string {
+// String is a stringer of Clock
+func (c Clock) String() string {
 	return fmt.Sprintf("%02d:%02d", c/60, c%60)
 }
 
-// Add(M) returns M minutes added to the clock instance
-func (c Clock)Add(m int) Clock {
-	return New(0, int(c) + m)
+// Add returns M minutes added to the clock instance
+func (c Clock) Add(m int) Clock {
+	return New(0, int(c)+m)
 }
 
-// Subtract(M) returns M minutes subtracted from the clock instance
-func (c Clock)Subtract(m int) Clock {
-	return New(0, int(c) - m)
+// Subtract returns M minutes subtracted from the clock instance
+func (c Clock) Subtract(m int) Clock {
+	return New(0, int(c)-m)
 }
