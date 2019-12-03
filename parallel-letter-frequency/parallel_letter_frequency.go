7fe03cab043d7ac422c5412190ac945e5137fa0e
condotti <akondo21@gmail.com>
2019-12-03 15:20:38 +0900

    about to submit (parallel-letter-frequency)

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -7,32 +7,28 @@ type FreqMap map[rune]int
 
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
-func Frequency(s string) FreqMap {
-	m := FreqMap{}
-	for _, r := range s {
-		m[r]++
+func Frequency(text string) FreqMap {
+	fm := FreqMap{}
+	for _, letter := range text {
+		fm[letter]++
 	}
-	return m
-}
-
-// worker is unexported function, which returns a FreqMap of individual text
-// over a channel.
-func worker(s string, c chan FreqMap) {
-	c <- Frequency(s)
+	return fm
 }
 
 // ConcurrentFrequency counts the frequency as function Frequency over
 // an array of texts.
 func ConcurrentFrequency(texts []string) FreqMap {
-	cs := make(chan FreqMap, len(texts))
+	c := make(chan FreqMap, len(texts))
 	for _, text := range texts {
-		go worker(text, cs)
+		go func(text string, c chan FreqMap) {
+			c <- Frequency(text)
+		}(text, c)
 	}
-	m := FreqMap{}
+	fm := FreqMap{}
 	for range texts {
-		for key, value := range <-cs {
-			m[key] += value
+		for letter, freq := range <-c {
+			fm[letter] += freq
 		}
 	}
-	return m
+	return fm
 }
