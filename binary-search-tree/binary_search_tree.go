6c6e19de3ff8d3b02ab4a80d0a8c0acab2dc4bf0
condotti <akondo21@gmail.com>
2019-12-19 15:20:12 +0900

    mod not to recurse (binary-search-tree)

--- binary-search-tree/binary_search_tree.go
+++ binary-search-tree/binary_search_tree.go
@@ -15,30 +15,45 @@ func Bst(data int) *SearchTreeData {
 
 // Insert is a method to insert a tree node to the tree.
 func (t *SearchTreeData) Insert(data int) {
-	if data > t.data {
-		if t.right == nil {
-			t.right = Bst(data)
+	target := t
+	for t != nil {
+		target = t
+		if data > t.data {
+			t = t.right
 		} else {
-			t.right.Insert(data)
+			t = t.left
 		}
+	}
+	if data > target.data {
+		target.right = Bst(data)
 	} else {
-		if t.left == nil {
-			t.left = Bst(data)
-		} else {
-			t.left.Insert(data)
-		}
+		target.left = Bst(data)
 	}
 }
 
 // MapString returns a slice of strings which is formatted by the function.
 func (t *SearchTreeData) MapString(f func(int) string) []string {
 	out := []string{}
-	if t.left != nil {
-		out = t.left.MapString(f)
-	}
-	out = append(out, f(t.data))
-	if t.right != nil {
-		out = append(out, t.right.MapString(f)...)
+	stack := []*SearchTreeData{t}
+	state := map[*SearchTreeData]int{}
+	for len(stack) > 0 {
+		switch state[stack[0]] {
+		case 0:
+			state[stack[0]]++
+			if stack[0].left != nil {
+				stack = append([]*SearchTreeData{stack[0].left}, stack...)
+			}
+		case 1:
+			state[stack[0]]++
+			out = append(out, f(stack[0].data))
+		case 2:
+			state[stack[0]]++
+			if stack[0].right != nil {
+				stack = append([]*SearchTreeData{stack[0].right}, stack...)
+			}
+		default:
+			stack = stack[1:]
+		}
 	}
 	return out
 }
@@ -46,12 +61,26 @@ func (t *SearchTreeData) MapString(f func(int) string) []string {
 // MapInt returns s slice of the tree data which is applied by the function.
 func (t *SearchTreeData) MapInt(f func(int) int) []int {
 	out := []int{}
-	if t.left != nil {
-		out = t.left.MapInt(f)
-	}
-	out = append(out, f(t.data))
-	if t.right != nil {
-		out = append(out, t.right.MapInt(f)...)
+	stack := []*SearchTreeData{t}
+	state := map[*SearchTreeData]int{}
+	for len(stack) > 0 {
+		switch state[stack[0]] {
+		case 0:
+			state[stack[0]]++
+			if stack[0].left != nil {
+				stack = append([]*SearchTreeData{stack[0].left}, stack...)
+			}
+		case 1:
+			state[stack[0]]++
+			out = append(out, f(stack[0].data))
+		case 2:
+			state[stack[0]]++
+			if stack[0].right != nil {
+				stack = append([]*SearchTreeData{stack[0].right}, stack...)
+			}
+		default:
+			stack = stack[1:]
+		}
 	}
 	return out
 }
