9d25a1d8b67566cc6dd7c17887f2fa63f5713fbc
condotti <akondo21@gmail.com>
2019-12-24 16:36:08 +0900

    loop version but incompleted (two-bucket)

--- two-bucket/two_bucket.go
+++ two-bucket/two_bucket.go
@@ -7,56 +7,77 @@ import (
 )
 
 // Solve returns a solution of the problem or an error if not found.
-// recursive version.
+// loop version.
 func Solve(bucketOne, bucketTwo, goal int, startBucket string) (goalBucket string, moves, otherBucket int, err error) {
-	history := map[string]bool{}
-	var step func(int, int, int)
-	step = func(contentOne, contentTwo, n int) {
-		// constraints: 0 <= content <= bucket, avoiding loop
-		state := fmt.Sprintf("%d,%d", contentOne, contentTwo)
-		if contentOne < 0 || contentOne > bucketOne || contentTwo < 0 || contentTwo > bucketTwo || history[state] {
-			return
-		}
-		// solution found
-		if contentOne == goal {
-			goalBucket, otherBucket, moves = "one", contentTwo, n
-			return
-		}
-		if contentTwo == goal {
-			goalBucket, otherBucket, moves = "two", contentOne, n
-			return
-		}
-		history[state] = true
-		// 1. pour one bucket to another
-		//   1.1 entire content to another
-		step(0, contentOne+contentTwo, n+1)
-		step(contentOne+contentTwo, 0, n+1)
-		//   1.2 a part of content to another
-		step(contentOne-bucketTwo+contentTwo, bucketTwo, n+1)
-		step(bucketOne, contentTwo-bucketOne+contentOne, n+1)
-		// 2. empty a bucket
-		step(0, contentTwo, n+1)
-		step(contentOne, 0, n+1)
-		// 3. fill a bucket
-		step(bucketOne, contentTwo, n+1)
-		step(contentOne, bucketTwo, n+1)
-		return
+	type frame struct { // stack frame
+		contentOne, contentTwo, moves, state int
 	}
+	stack := []*frame{}
+	push := func(c1, c2, m int) {
+		stack = append([]*frame{&frame{c1, c2, m + 1, 0}}, stack...)
+	}
+	pop := func() {
+		stack = stack[1:]
+	}
+
+	history := map[string]bool{}
 	goalBucket, err = "", nil
 	if bucketOne <= 0 || bucketTwo <= 0 || goal <= 0 {
 		err = errors.New("invalid bucket size or goal amount")
 	}
+
 	switch startBucket {
 	case "one":
-		step(bucketOne, 0, 1)
+		push(bucketOne, 0, 1)
 	case "two":
-		step(0, bucketTwo, 1)
+		push(0, bucketTwo, 1)
 	default:
 		err = errors.New("invalid start bucket name")
+		return
 	}
-	if goalBucket == "" {
-		err = errors.New("no solution found")
+
+	for len(stack) > 0 {
+		top := stack[0]
+		switch top.state {
+		case 0: // constraints: 0 <= content <= bucket, avoiding loop
+			state := fmt.Sprintf("%d,%d", top.contentOne, top.contentTwo)
+			if top.contentOne < 0 || top.contentOne > bucketOne ||
+				top.contentTwo < 0 || top.contentTwo > bucketTwo ||
+				history[state] {
+				pop()
+			}
+			history[state] = true
+		case 1: // solution found
+			switch {
+			case top.contentOne == goal:
+				goalBucket, otherBucket, moves = "one", top.contentTwo, top.moves
+				return
+			case top.contentTwo == goal:
+				goalBucket, otherBucket, moves = "two", top.contentOne, top.moves
+				return
+			}
+		case 2: // pour entire content to another
+			push(0, top.contentOne+top.contentTwo, top.moves)
+		case 3:
+			push(top.contentOne+top.contentTwo, 0, top.moves)
+		case 4: // pour a part of content to another
+			push(top.contentOne-(bucketTwo-top.contentTwo), bucketTwo, top.moves)
+		case 5:
+			push(bucketOne, top.contentTwo-(bucketOne-top.contentOne), top.moves)
+		case 6: // empty a bucket
+			push(0, top.contentTwo, top.moves)
+		case 7:
+			push(top.contentOne, 0, top.moves)
+		case 8: // fill a bucket
+			push(bucketOne, top.contentTwo, top.moves)
+		case 9:
+			push(top.contentOne, bucketTwo, top.moves)
+		default:
+			pop()
+		}
+		top.state++
 	}
+	err = errors.New("no solution found")
 	return
 }
 
