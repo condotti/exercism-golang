e55f6ddf0df916278b36a5beb45f16484d69cc2b
condotti <akondo21@gmail.com>
2020-01-22 16:31:40 +0900

    new solution (ocr-numbers)

---
+++ ocr-numbers/ocr_numbers.go
@@ -0,0 +1,64 @@
+// Package ocr implements a solution of the exercise titled `OCR Numbers'.
+package ocr
+
+import (
+	"strconv"
+	"strings"
+)
+
+func partition(s string, l int) []string {
+	parted := []string{}
+	for ; len(s) >= l; s = s[l:] {
+		parted = append(parted, s[:l])
+	}
+	return parted
+}
+
+func chunkOff(s []string) []string {
+	chunks := []string{}
+	for _, line := range s {
+		for i, chunk := range partition(line, 3) {
+			if len(chunks) <= i {
+				chunks = append(chunks, "")
+			}
+			chunks[i] += chunk
+		}
+	}
+	return chunks
+}
+
+var pattern []string = chunkOff(strings.Split(`
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|
+                              `, "\n"))
+
+func recognizeDigit(s string) string {
+	for i, pat := range pattern {
+		if s == pat {
+			return strconv.Itoa(i)
+		}
+	}
+	return "?"
+}
+
+func recognizeLine(s []string) string {
+	digits := ""
+	for _, chunk := range chunkOff(s) {
+		digit := recognizeDigit(chunk)
+		digits += digit
+
+	}
+	return digits
+}
+
+// Recognize returns a recognized ocr number as a string.
+func Recognize(in string) []string {
+	out := []string{}
+	lines := strings.Split(in, "\n")[1:]
+	for len(lines) > 0 {
+		out = append(out, recognizeLine(lines[:4]))
+		lines = lines[4:]
+	}
+	return out
+}
